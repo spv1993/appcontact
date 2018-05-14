a7908f2fa5f1450e5a47a302400c835f3fb88fa5
spv1993 <pavel.shalda93@gmail.com>
2018-05-14 15:04:08 +0700

    oauth2 basic

--- src/main/java/edu/springproject/appcontact/conf/WebMvcConfig.java
+++ src/main/java/edu/springproject/appcontact/conf/WebMvcConfig.java
@@ -1,17 +1,13 @@
 package edu.springproject.appcontact.conf;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
- 
+
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
-     
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-  
+@ComponentScan(basePackages = "edu.springproject.appcontact")
+public class WebMvcConfig {
+	
+
 }
---
+++ src/main/java/edu/springproject/appcontact/controller/AuthenticationController.java
@@ -0,0 +1,12 @@
+package edu.springproject.appcontact.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class AuthenticationController {
+
+//	@PostMapping("/contacts")
+}
